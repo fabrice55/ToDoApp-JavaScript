790db00023229788d9a8438c5d9f4653020dd863
fabrice <fabrice5512@hotmail.com>
2025-02-15 16:16:57 +0200

    added index.js

--- api/delete-item.js
+++ api/delete-item.js
@@ -1,6 +1,6 @@
 const db = require('../db')
 const {ObjectId} = require('mongodb')
-const sanitizeHTML = require('sanitize-html')
+
 
 exports.delete = async function(req, res) {
     await db.db().collection("items").deleteOne({_id: new ObjectId(req.body.id)}).then(function() {
--- router-api.js
+++ router-api.js
@@ -2,10 +2,12 @@ const apiRouter = require("express").Router()
 const createItem = require('./api/create-item')
 const updateItem = require('./api/update-item')
 const deleteItem = require('./api/delete-item')
+const viewItems = require('./api/index')
 
 apiRouter.post('/create-item', createItem.apiCreate)
 apiRouter.post('/update-item', updateItem.apiUpdate)
 apiRouter.post('/delete-item', deleteItem.apiDelete)
+apiRouter.post('/view-items', viewItems.apiViewListOfItems)
 
 
 module.exports = apiRouter
