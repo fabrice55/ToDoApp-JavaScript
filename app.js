d2729c7833aff998af37df96be26ee18c3dd6602
fabrice <fabrice5512@hotmail.com>
2025-02-17 13:16:42 +0200

    modified app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const db = require('./db')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-//app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
+//app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', routerApi)
 
