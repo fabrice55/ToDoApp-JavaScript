fcd3468325ff062c681fba70fc9115af3e903cd0
fabrice <fabrice5512@hotmail.com>
2025-02-17 13:22:47 +0200

    changed how app.js accesses static files in the public folder

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const db = require('./db')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static('public'))
-//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', routerApi)
 
