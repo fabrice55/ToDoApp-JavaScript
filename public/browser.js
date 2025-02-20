378329b010c7278e2cb8677822ddeb2282cefeb9
fabrice <fabrice5512@hotmail.com>
2025-02-20 18:48:49 +0200

    modified browser.js edit behaviour

--- public/browser.js
+++ public/browser.js
@@ -49,7 +49,7 @@ document.addEventListener("click", function(e) {
 //Update Feature
 
     if (e.target.classList.contains("edit-me")) {
-        let userInput = prompt("Enter your desired new text")
+        let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
         if (userInput) {
             axios.post('/update-item',{text: userInput, id: e.target.getAttribute("data-id")}).then(function () {
                 e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
